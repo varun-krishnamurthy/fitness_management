dab5bfd3118d71540b1f85e1e31d40a55fc22127
Your Name <you@example.com>
2026-05-12 05:59:22 +0000

    Add missing DocType JSON and Python files for all modules

---
+++ fitness_management/fitness_management/doctype/diet_log/diet_log.py
@@ -0,0 +1,5 @@
+import frappe
+from frappe.model.document import Document
+
+class Dietlog(Document):
+    pass
---
+++ fitness_management/fitness_management/doctype/meal_item/meal_item.py
@@ -0,0 +1,5 @@
+import frappe
+from frappe.model.document import Document
+
+class Mealitem(Document):
+    pass
